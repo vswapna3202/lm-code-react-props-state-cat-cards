a27f9a2f1390026c40cec836e5c8fb6f5f13ad7b
vswapna3202 <vswapna3202@gmail.com>
2023-10-17 14:15:14 +0100

    Feat: Added code for activity-1

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,106 @@ import './App.css';
 import Navbar from './components/navbar';
 import Header from './components/header';
 import Footer from './components/footer';
+import { useState } from 'react';
+import Cat from './data/cat';
 
-function App(): JSX.Element {
+function App() {
 
 	// JavaScript/TypeScript code can be inserted here!
-
+	const [ cats, setCats ] = useState<Array<Cat>>(
+		[
+			{
+					 name: 'Little Miss Purrfect',
+					 species: 'Cat',
+					 favFoods: ['wet food', 'dry food'],
+					 birthYear: 2016,
+				 },
+				 {
+					 name: 'Mr Gwumpy',
+					 species: 'Cat',
+					 favFoods: ['caviar'],
+					 birthYear: 2016,
+				 },
+				 {
+					 name: 'Dopey',
+					 species: 'Cat',
+					 favFoods: ['bugs'],
+					 birthYear: 2018,
+				 },
+				 {
+					 name: 'Jumpy',
+					 species: 'Scaredy Cat',
+					 favFoods: ['not cucumbers!'],
+					 birthYear: 2008,
+				 },
+				 {
+					 name: 'Meowsalot',
+					 species: 'Cat',
+					 favFoods: ['tuna', 'catnip', 'celery'],
+					 birthYear: 2012,
+				 },
+				 {
+					 name: 'Cindy Clawford',
+					 species: 'Cat',
+					 favFoods: ['mice'],
+					 birthYear: 2012,
+				 },
+				 {
+					 name: 'Katy Purry',
+					 species: 'Cat',
+					 favFoods: ['cigarettes', 'coffee'],
+					 birthYear: 2015,
+				 },
+				 {
+					 name: 'Dr. Von Belly-Rub',
+					 species: 'Cat',
+					 favFoods: ['salt'],
+					 birthYear: 2020,
+				 },
+				 {
+					 name: 'Blobby',
+					 species: 'Bird?',
+					 favFoods: ['your soul'],
+					 birthYear: 2020,
+				 },
+				 {
+					 name: 'El Taco',
+					 species: 'Cat',
+					 favFoods: ['tequila', 'bar snax'],
+					 birthYear: 1995,
+				 },
+				 {
+					 name: 'Nibbles',
+					 species: 'Hungry Cat',
+					 favFoods: ['blankets', 'feet', 'tinsel'],
+					 birthYear: 2003,
+				 },
+				 {
+					 name: 'BoopySnoot',
+					 species: 'Tiny Cat',
+					 favFoods: ['milk'],
+					 birthYear: 2021,
+				 }
+		 ]
+	);
+	console.log("Our pretties 😻: ", cats);
 	return (
 		<>
 			<Navbar />
 			<Header />
 
 			<main>
-				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
+				<div className='cards__wrapper'>		  
+						{cats.map(cat => (		
+						  <div className="card" key="{cat.name}">					
+							<h3 className="card__text card__header">{cat.name}</h3>	
+							<img className="card__image" src="./img/cuteTabbyCats.jpg" alt="A cute tabby kitten"></img>
+							<p>Species:{cat.species}</p>
+							<p>Favourite Foods:{cat.favFoods.join(', ')}</p>
+							<p>Birth Year:{cat.birthYear}</p>	
+						  </div>								
+						))}							
+				</div>
 			</main>
 
 			<Footer />
